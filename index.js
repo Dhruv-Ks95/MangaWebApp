65b03ecb5935d5d638102ddc08c1e43fe18d003c
Dhruv-Ks95 <vinit.dhruv21@vit.edu>
2024-02-19 00:20:27 +0530

    Added some code for server and UI

--- index.js
+++ index.js
@@ -6,8 +6,21 @@ const port = 4150;
 app.use(express.urlencoded({extended:true}));
 app.use(express.static("public"));
 
-app.get("/", (req,res)=>{
-    res.render("index.ejs");
+// Random API until i get access to MangaDex API
+const API_URL = "https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=12322";
+
+app.get("/", async(req,res)=>{
+    try{
+        const response = await axios.get(API_URL);
+        console.log(response.data.drinks);
+        const drinkname = response.data.drinks[0].strDrink;
+        const drinkurl = response.data.drinks[0].strDrinkThumb;
+        console.log(drinkname,drinkurl);
+        res.render("index.ejs", {dname:drinkname,durl:drinkurl});
+    }catch(error){
+        console.log("some error in fetching from API");
+        res.sendStatus(500);
+    }
 });
 
 app.get("/aboutus.ejs", (req,res)=>{
