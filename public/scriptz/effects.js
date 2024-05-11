48af176d195461ed215874876f835904105b00e5
Dhruv Kshirasagar <vinit.dhruv21@vit.edu>
2024-05-11 14:09:51 +0530

    improved styling

--- public/scriptz/effects.js
+++ public/scriptz/effects.js
@@ -1,1 +1,7 @@
-// alert("How are ya ?");
+$(".readmore").click(function(){
+    $(this).closest(".thecard").css("transform","rotateY(180deg)");
+});
+
+$(".goback").click(function(){
+    $(this).closest(".thecard").css("transform","");
+});
